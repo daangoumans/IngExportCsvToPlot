7f65adc0a94c30eb01513dd5ea24c1035b39d31b
Daan Goumans <daan.goumans@hotmail.com>
2017-09-17 19:10:26 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dates = []
+amount = []
+df = pd.read_csv("ing_export.csv")
+
+
+for index, row in df.iterrows():
+    if "Af" in row['Af Bij']:
+        row["Bedrag (EUR)"] = ("-" + row["Bedrag (EUR)"])
+    bedrag = float(row["Bedrag (EUR)"].replace(",","."))
+    dates.append(pd.to_datetime(str(row["Datum"])[:6], format='%Y%m'))
+    amount.append(bedrag)
+new_df = list(zip(dates,amount))
+
+df = pd.DataFrame(data = new_df, columns=['Date', 'Amount'])
+df = df.groupby(["Date"])[["Amount"]].sum()
+plt.figure()
+df.plot()
+plt.savefig('plot')
